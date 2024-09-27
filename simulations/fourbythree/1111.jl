4bb03ef70139f8a632165a7c9709600ad9139731
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2024-09-27 15:13:43 +0200

    adding distributions dep

--- simulations/fourbythree/1111.jl
+++ simulations/fourbythree/1111.jl
@@ -5,7 +5,7 @@ using Plots
 
 Random.seed!(20240925)
 
-sims = 1000
+sims = 50
 n = [4, 3]
 ranks = [1, 1]
 
@@ -13,12 +13,10 @@ maxiters = 50
 ϵ = 1e-02
 p = 0
 
-smallaic = fill(NaN, 4, sims)
-smallbic = fill(NaN, 4, sims)
-smallhqc = fill(NaN, 4, sims)
-medaic = fill(NaN, 4, sims)
-medbic = fill(NaN, 4, sims)
-medhqc = fill(NaN, 4, sims)
+firstsmallic = fill(NaN, 3, sims)
+firstmedic = fill(NaN, 3, sims)
+secondsmallic = fill(NaN, 3, sims)
+secondmedic = fill(NaN, 3, sims)
 
 trueU1 = fill(NaN, n[1], ranks[1])
 trueU2 = fill(NaN, n[2], ranks[2])
@@ -46,19 +44,65 @@ for i in 1:1000
 end
 mecmstable(trueU1, trueU2, trueU3, trueU4, trueϕ1, trueϕ2)
 
-smallobs = 100
+# smallobs = 100
 medobs = 500
 burnin = 100
 
-smallmecm = generatemecmdata(trueU1, trueU2, trueU3, trueU4, trueϕ1, trueϕ2, smallobs; burnin=burnin)
-smalldata = smallmecm.mardata
-smallloglike = smallmecm.ll
+for s in ProgressBar(1:sims)
+    # smallmecm = generatemecmdata(trueU1, trueU2, trueU3, trueU4, trueϕ1, trueϕ2, smallobs; burnin=burnin)
+    # smalldata = smallmecm.mardata
+    # smallloglike = smallmecm.ll
 
-medmecm = generatemecmdata(trueU1, trueU2, trueU3, trueU4, trueϕ1, trueϕ2, medobs; burnin=burnin)
-meddata = medmecm.mardata
-medloglike = medmecm.ll
+    # aicsmall, bicsmall, hqcsmall = selectmecm(smalldata; p, maxiters, ϵ)
+    # firstsmallic[1, s] = aicsmall[1]
+    # firstsmallic[2, s] = bicsmall[1]
+    # firstsmallic[3, s] = hqcsmall[1]
+    #
+    # secondsmallic[1, s] = aicsmall[2]
+    # secondsmallic[2, s] = bicsmall[2]
+    # secondsmallic[3, s] = hqcsmall[2]
 
-aicsmall, bicsmall, hqcsmall = selectmecm(smalldata; p, maxiters, ϵ)
-aicmed, bicmed, hqcmed = selectmecm(meddata; p, maxiters, ϵ)
+    medmecm = generatemecmdata(trueU1, trueU2, trueU3, trueU4, trueϕ1, trueϕ2, medobs; snr=0.7)
+    # df = randn(size(medmecm.data))
+
+    aicmed, bicmed, hqcmed, ictable = selectmecm(medmecm.data; p=p, maxiters=100, ϵ=ϵ)
+    firstmedic[1, s] = aicmed[1]
+    firstmedic[2, s] = bicmed[1]
+    firstmedic[3, s] = hqcmed[1]
+
+    secondmedic[1, s] = aicmed[2]
+    secondmedic[2, s] = bicmed[2]
+    secondmedic[3, s] = hqcmed[2]
+end
+
+# xx = tenmat(medmecm.data, row=[1, 2]) * tenmat(medmecm.data, row=[1, 2])'
+# plot(reverse(abs.(eigvals(xx ./ medobs))))
+
+mean(firstmedic, dims=2)
+mean(secondmedic, dims=2)
+
+obs = 500
+genmecm = generatemecmdata(trueU1, trueU2, trueU3, trueU4, trueϕ1, trueϕ2, obs; burnin=burnin)
+results = mecm(genmecm.data, [1, 1]; p=0, maxiter=50, ϵ=1e-02)
+results.llist[1:findlast(!isnan, results.llist)]
+startidx = 1
+plot(results.llist[startidx:findlast(!isnan, results.llist)])
+plot(results.fullgrads)
+
+results.U2 / results.U2[1]
+trueU2 / trueU2[1]
+
+svdvals(results.U1)
+svdvals(results.U2)
+svdvals(results.U3)
+svdvals(results.U4)
+
+fac1 = fill(NaN, 1, 3, obs)
+for i in 1:obs
+    fac1[:, :, i] .= results.U3' * genmecm.data[:, :, i]
+end
+plot(tenmat(fac1, row=[1, 2])')
+
+
 
 
