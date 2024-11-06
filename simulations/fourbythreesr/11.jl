9fc4f1c1d54c17015cdaa64cc2891b762df09128
Ivan Ricardo <iu.ricardo@maastrichtuniversity.nl>
2024-11-06 22:15:07 +0100

    11 works now

--- simulations/fourbythreesr/11.jl
+++ simulations/fourbythreesr/11.jl
@@ -3,7 +3,7 @@ using DrWatson
 using TensorToolbox, Statistics, Random, LinearAlgebra, CommonFeatures, ProgressBars
 using Plots, DelimitedFiles, Latexify
 
-Random.seed!(20241104)
+Random.seed!(20241108)
 
 sims = 1000
 n = [3, 4]
